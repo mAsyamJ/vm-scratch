c7e92bb80af7392174434240081ebbf792119b9b
mAsyamJ <jayanegara.asyam@gmail.com>
2025-10-03 21:53:08 +0700

    JUMP

--- miniEVM/evmState.py
+++ miniEVM/evmState.py
@@ -529,6 +529,45 @@ class Opcodes:
     evm.gas_dec(100)
     evm.pc += 1
 
+    # =========================== JUMP =============================
+    def jump(evm):
+    counter = evm.stack.pop()
+
+    # make sure that we jump to an JUMPDEST opcode
+    if not evm.program[counter] == JUMPDEST:
+        raise Exception("Can only jump to JUMPDEST")
+
+    evm.pc = counter
+    evm.gas_dec(8)
+
+    def jumpi(evm):
+    counter, b = evm.stack.pop(), evm.stack.pop()
+
+    if b != 0: evm.pc = counter
+    else     : evm.pc += 1
+    
+    evm.gas_dec(10)
+
+    def pc(evm):
+    evm.stack.push(evm.pc)
+    evm.pc += 1
+    evm.gas_dec(2)
+
+    def jumpdest(evm):
+    evm.pc += 1
+    evm.gas_dec(1)
+
+    # Push
+    def _push(evm, n):
+    evm.pc += 1
+    evm.gas_dec(3)
+    
+    value = []
+    for _ in range(n):
+        value.append(evm.peek())
+        evm.pc += 1
+    evm.stack.push(int(''.join(map(str, value))))
+
 
 
 class State:
